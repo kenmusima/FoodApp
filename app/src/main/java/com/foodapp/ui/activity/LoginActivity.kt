4084751b90cc37a3fcbe5d17c4fca4269176f18e
kenmusima <ksmusima@gmail.com>
2022-08-16 19:24:35 +0300

    :sparkles: Create Login UI
    Create xmls for UI

---
+++ app/src/main/java/com/foodapp/ui/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.foodapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodapp.R
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
