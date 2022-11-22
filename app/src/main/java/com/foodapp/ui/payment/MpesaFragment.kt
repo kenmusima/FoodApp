7880690749aa89e104908829306747b07866defb
kenmusima <ksmusima@gmail.com>
2022-11-22 20:52:30 +0300

    Payment options layout

---
+++ app/src/main/java/com/foodapp/ui/payment/MpesaFragment.kt
@@ -0,0 +1,8 @@
+package com.foodapp.ui.payment
+
+import androidx.fragment.app.Fragment
+import com.foodapp.R
+
+class MpesaFragment : Fragment(R.layout.fragment_mpesa) {
+
+}
---
+++ app/src/main/java/com/foodapp/ui/payment/VisaFragment.kt
@@ -0,0 +1,8 @@
+package com.foodapp.ui.payment
+
+import androidx.fragment.app.Fragment
+import com.foodapp.R
+
+class VisaFragment : Fragment(R.layout.fragment_visa) {
+
+}
